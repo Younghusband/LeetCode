17a637fe8b0ea369e938376ef67fe4017173d813
vermouth.yf <vermouth.yf@gmail.com>
2017-02-07 22:32:13 +0800

    测试用mac版本的eclipse完成 git的相关操作  @master

---
+++ src/com/yangfan/code/TestMAMAMA.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.yangfan.code;
+/**
+ * @description 
+ * @author vermouth.Mac
+ * @version 2017年2月7日 下午10:30:46
+ */
+
+/**
+ * @author Younghusband
+ *
+ */
+public class TestMAMAMA {
+
+}
