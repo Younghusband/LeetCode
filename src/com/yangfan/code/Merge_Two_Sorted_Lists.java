ef98724ae7d0674ec8287b8de4cd626d1474fa7c
vermouth.yf <vermouth.yf@gmail.com>
2017-02-23 14:11:11 +0800

    增加几个链表的实现类  修改一下merge two list的答案 @master

--- src/com/yangfan/code/Merge_Two_Sorted_Lists.java
+++ src/com/yangfan/code/Merge_Two_Sorted_Lists.java
@@ -24,30 +24,23 @@ class ListNode{
 public class Merge_Two_Sorted_Lists {
 	
 	public static void main(String[] args) {
-		//该怎么写测试用例呢... 
+		
+		
 	}
 	
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null)return l2;
-        if(l2==null)return l1;
-        
-        ListNode cur = null;
+        if(l1==null) return l2;
+        if(l2==null) return l1;
         
         if(l1.val<l2.val){
-        	cur = l1;
-        	cur.next = mergeTwoLists(l1.next,l2);
+        	l1.next = mergeTwoLists(l1.next,l2);
+        	return l1;
         }else{
-        	cur = l2;
-        	cur.next = mergeTwoLists(l1,l2.next);
+        	l2.next = mergeTwoLists(l1,l2.next);
+        	return l2;
         }
-    	return cur;
     }
     
-    public ListNode mergeTwoLists_fast(ListNode l1,ListNode l2){
-        
-    	
-    	return null;
-    }
     
     
 
