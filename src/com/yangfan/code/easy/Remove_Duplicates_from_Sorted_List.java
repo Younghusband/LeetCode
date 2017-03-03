79fbf8621f6b598165b19273241f8aba2e29e219
vermouth.yf <vermouth.yf@gmail.com>
2017-03-03 17:58:02 +0800

    2017/03/03 作业截图 @master

---
+++ src/com/yangfan/code/easy/Remove_Duplicates_from_Sorted_List.java
@@ -0,0 +1,42 @@
+package com.yangfan.code.easy;
+
+import com.yangfan.code.easy.SortedList.ListNode;
+
+/************************************************
+    * Description: 
+    *    Given a sorted linked list, delete all duplicates such that each element appear only once.
+         For example,
+         Given 1->1->2, return 1->2.
+         Given 1->1->2->3->3, return 1->2->3. 
+    * 
+    * @author    Vermouth.yf  
+    * @version  1.0
+    * @date ：2017年3月3日 下午1:08:35 
+**************************************************/
+public class Remove_Duplicates_from_Sorted_List {
+       public static void main(String[] args) {
+		
+	}
+       
+       public ListNode deleteDuplicates(ListNode head) {
+           
+    	   return head;
+       }
+}
+
+
+/**
+ * insert deleteFirst display
+ *
+ */
+class SortedList{
+   class ListNode {
+	      int val;
+	      ListNode next;
+	      ListNode(int x) { val = x; }
+	  }
+   
+   
+   
+   
+}
