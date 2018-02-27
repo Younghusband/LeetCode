0a53a8c06b7455b9d0cf1d87ccddbc339f356372
vermouth.yf <vermouth.yf@gmail.com>
2018-02-27 20:42:15 +0800

    更改一下死锁的写法

--- src/com/playground/thread/lock/DeadLock.java
+++ src/com/playground/thread/lock/DeadLock.java
@@ -8,50 +8,48 @@ package com.playground.thread.lock;
  */
 
 public class DeadLock {
-    public static String obj1 = "obj1";
-    public static String obj2 = "obj2";
-    public static void main(String[] args){
-        Thread a = new Thread(new Lock1(),"Lock1");
-        Thread b = new Thread(new Lock2(),"Lock2");
-        a.start();
-        b.start();
-    }    
-}
-class Lock1 implements Runnable{
-    @Override
-    public void run(){
-        try{
-            System.out.println("Lock1 running");
-//            while(true){
-                synchronized(DeadLock.obj1){   //lock1 锁住了obj1 等待obj2
-                    System.out.println(Thread.currentThread().getName()+" lock obj1");
-                    Thread.sleep(3000);//获取obj1后先等一会儿，让Lock2有足够的时间锁住obj2
-                    synchronized(DeadLock.obj2){  //双方卡在这里
-                        System.out.println(Thread.currentThread().getName()+" lock obj2");
-                    }
-                }
-//            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-}
-class Lock2 implements Runnable{
-    @Override
-    public void run(){
-        try{
-            System.out.println("Lock2 running");
-//            while(true){
-                synchronized(DeadLock.obj2){   //lock2锁住了obj2 等待obj1
-                    System.out.println(Thread.currentThread().getName()+" lock obj2");
-                    Thread.sleep(3000);
-                    synchronized(DeadLock.obj1){
-                        System.out.println(Thread.currentThread().getName()+" lock obj1");
-                    }
-                }
-//            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
+	
+	public static void main(String[] args) {
+		Object lock1 = new Object();
+		Object lock2 = new Object();
+		
+		Thread t1 = new Thread(new Runnable() {
+			public void run() {
+				synchronized (lock1) {
+                    System.out.println("t1 entering...have lock1");
+//                    try {
+//						Thread.sleep(3000);
+//					} catch (InterruptedException e) {
+//						e.printStackTrace();
+//					}
+					synchronized(lock2){
+						System.out.println("t1 have lock2...");
+						
+					}
+					
+				}
+			}
+		}
+
+				, "t1");
+		
+		
+		Thread t2 = new Thread(new Runnable() {
+			public void run() {
+				synchronized (lock2) {
+					 System.out.println("t2 entering...have lock2");
+						synchronized(lock1){
+							System.out.println("t2 have lock1...");
+						}
+				}
+			}
+		}
+
+				, "t2");
+		
+		t1.start();
+		t2.start();
+		
+	}
+
 }
