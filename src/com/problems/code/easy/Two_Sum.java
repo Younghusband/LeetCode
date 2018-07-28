eb5d2d99dc5337dd767630c3448f6d9e1a87b8e1
mr_younghusband <mr_younghusband@yeah.net>
2018-07-28 15:17:36 +0800

    commit two_sum

--- src/com/problems/code/easy/Two_Sum.java
+++ src/com/problems/code/easy/Two_Sum.java
@@ -1,4 +1,8 @@
 package com.problems.code.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /************************************************
     * Description: 
     *   Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -13,51 +17,86 @@ package com.problems.code.easy;
 **************************************************/
 public class Two_Sum {
 	
-	public static void main(String[] args) {
-		int [] arr = {2, 7, 11, 15};
-		arr = twoSum666(arr,9);
-		for( int i : arr)
-		   System.out.print(i+"->");
-		
-	}
-	
-	public static int[] twoSum(int[] nums, int target) {
-        int len = nums.length;
-    	int a=0,b=0;   //待加的
-    	boolean flag = false;
-    	
-    	for(int i=0;i<len-1;i++){
-    		for(int j=i+1;j<len;j++){
-    			if((nums[i]+nums[j])==target){
-    				a = i;
-    				b = j;
-    				flag = true;
-    			}
-    		}
-    	}
-    	if(!flag){
-    		throw null;	
-    	}
-        int [] result = new int[] {a,b};
-    	return result;
+    public static void main(String[] args) {
+        int[] arr = { 2, 7, 11, 15 };
+        arr = twoSum666(arr, 9);
+        for (int i : arr)
+            System.out.print(i + "->");
     }
-	
-	
-	
-	/**
-	 * 速度超快这种
-	 */
-	public static int[] twoSum666(int[] nums, int target) {
-        int[] map = new int[16030];
+
+    /**
+     * Brute-force
+     * 
+     */
+    public static int[] twoSum_brute(int[] nums, int target) {
+        int len = nums.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     *  two-pass hashTable   
+     * 
+     */
+    public static int[] twoSum_twopass(int[] nums, int target) {
+        Map<Integer, Integer> value_index = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-           int diff=target-nums[i]+5;				//给了一个5的缓冲
-           if (diff < 0) continue;				//不在范围内
-           if(map[diff]>0){					//在map数组中
-               return new int[] {map[diff]-1,i};
-           }
-           map[nums[i]+5]=i+1;					//放入map数组中
+            value_index.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int c = target - nums[i];
+            if (value_index.containsKey(c) && value_index.get(c) != i) { //防止当前遍历元素刚好是target的一半而返回自身坐标
+                return new int[] { i, value_index.get(c) };
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution.");
+    }
+
+    /**
+     * one-pass hashTable
+     * 
+     * 边放边找的思路是很赞的
+     */
+    public static int[] twoSum_onepass(int[] nums, int target) {
+        Map<Integer, Integer> value_index = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int c = target - nums[i];
+            if (value_index.containsKey(c) && value_index.get(c) != i) {
+                return new int[] { i, value_index.get(c) };
+            } else {
+                value_index.put(nums[i], i);
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution.");
+    }
+
+    /**
+     * 速度超快这种
+     * 
+     * 当时在论坛里找到的方法，现在已经找不到原帖子了。
+     * 
+     * 但是由于数组过小，在新的测试用例下 这个是AC不了的
+     */
+    public static int[] twoSum666(int[] nums, int target) {
+        int[] map = new int[16030]; // 62kb
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i] + 5; // 给了一个5的缓冲
+            if (diff < 0)
+                continue; // 不在范围内
+            if (map[diff] > 0) { // 在map数组中
+                return new int[] { map[diff] - 1, i };
+            }
+            map[nums[i] + 5] = i + 1; // 放入map数组中
         }
         throw null;
-}
+    }
 
 }
