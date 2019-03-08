a019732ae7a124427884af32c7e94d1e38ad6dab
vermouth.yf <vermouth.yf@gmail.com>
2019-03-08 23:07:37 +0800

    鍚堝苟鍐茬獊

--- src/com/playground/Main.java
+++ src/com/playground/Main.java
@@ -1,5 +1,7 @@
 package com.playground;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -7,8 +9,14 @@ import java.util.concurrent.atomic.AtomicInteger;
  * playground
  */
 public class Main {
-    
+
+    public static ConcurrentHashMap<String, AtomicInteger> concurrents = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
+
     }
+
+
+
 
 }
