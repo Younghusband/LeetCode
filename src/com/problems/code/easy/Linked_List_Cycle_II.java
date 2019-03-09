a7ba1671e0743fea3acab2cee38cf29fe4df67de
mr_younghusband <mr_younghusband@yeah.net>
2019-03-09 14:26:56 +0800

    merge two lists

--- src/com/problems/code/easy/Linked_List_Cycle_II.java
+++ src/com/problems/code/easy/Linked_List_Cycle_II.java
@@ -4,6 +4,15 @@ package com.problems.code.easy;
  * @author mr_yo
  * 判断链表中是否有环进阶版
  * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ * 
+ * 
+ * 假设从head到环入口的距离为 Len
+ *     环入口到快慢节点相遇点的距离为 x
+ *     环的长度为R
+ *     快节点在环内转了n圈
+ *     那么可以推导出   Len = nR-x
+ *     推导过程见博客
+ *     所以，设置一个从head开始跑的慢节点与在环内相遇点的慢节点同时跑，最终会在入口处相遇(理解一下nR-x, 就懂了)
  */
 public class Linked_List_Cycle_II {
     
@@ -18,7 +27,6 @@ public class Linked_List_Cycle_II {
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            
             if(fast == slow) {
                 ListNode slow2 = head;
                 while(slow2!=slow) {
@@ -30,5 +38,6 @@ public class Linked_List_Cycle_II {
         }
         return null;
     }
-
+    
+    
 }
--- src/com/problems/code/easy/Merge_Two_Sorted_Lists.java
+++ src/com/problems/code/easy/Merge_Two_Sorted_Lists.java
@@ -5,12 +5,8 @@ package com.problems.code.easy;
     * Merge two sorted linked lists and return it as a new list. 
     * The new list should be made by splicing together the nodes of the first two lists.
     * 
-    * method 1: common solution
-    * method 2: use recursion
-    * 
-    * @author    Vermouth.yf  
-    * @version  1.0
-    * @date ：2017年2月4日 上午10:22:15 
+    * 1. 递归方式
+    * 2. 利用哑节点头
 **************************************************/
 
 class ListNode{
@@ -25,7 +21,15 @@ public class Merge_Two_Sorted_Lists {
 	
 	public static void main(String[] args) {
 		//哈哈
-		
+	    Merge_Two_Sorted_Lists service = new Merge_Two_Sorted_Lists();
+	    ListNode l1 = null;
+	    ListNode l2 = new ListNode(0);
+	    
+	    ListNode x = service.mergeTwoListsPro(l1, l2);
+	    while(x!=null) {
+	        System.out.println(x.val);
+	        x = x.next;
+	    }
 	}
 	
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
@@ -39,6 +43,40 @@ public class Merge_Two_Sorted_Lists {
         	l2.next = mergeTwoLists(l1,l2.next);
         	return l2;
         }
+    }
+    
+    
+    public ListNode mergeTwoListsPro(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode cur = dummyHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                cur = cur.next;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                cur = cur.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 == null) {
+            cur.next = l2;
+        } else {
+            cur.next = l1;
+        }
+        return dummyHead.next;
+    }
+    
+    
+    /**
+     * 最好的方法
+     */
+    public ListNode mergeTwoListsBest(ListNode l1, ListNode l2) {
+        
+        
+        return null;
     }
     
     
