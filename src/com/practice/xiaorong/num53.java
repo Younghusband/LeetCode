e098648e0b94e47cff7c6f1fad5cbd507adc6c06
徐小荣 <xuxiaorong@xuxiaorongdeMBP.lan>
2020-03-11 00:16:17 +0800

    num53

---
+++ src/com/practice/xiaorong/num53.java
@@ -0,0 +1,17 @@
+package com.practice.xiaorong;
+
+public class num53 {
+
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int maxSum = nums[0];
+        int currentMax = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            maxSum = Math.max(currentMax, maxSum);
+        }
+        return maxSum;
+    }
+}
