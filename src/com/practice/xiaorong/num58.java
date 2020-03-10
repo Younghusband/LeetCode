786345ebf339adbb37a376c41a16fa4762837c52
徐小荣 <xuxiaorong@xuxiaorongdeMBP.lan>
2020-03-11 00:30:49 +0800

    num58

---
+++ src/com/practice/xiaorong/num58.java
@@ -0,0 +1,24 @@
+package com.practice.xiaorong;
+
+public class num58 {
+    public static void main(String[] args) {
+        lengthOfLastWord("hello world");
+    }
+    public static int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        if (index < 0) {
+            return 0;
+        }
+        int count = 0;
+        do {
+            if (s.charAt(index) == ' ' && count == 0) {
+                continue;
+            } else if (s.charAt(index) == ' ') {
+                return count;
+            } else{
+                count ++;
+            }
+        } while ((index--) > 0);
+        return count;
+    }
+}
